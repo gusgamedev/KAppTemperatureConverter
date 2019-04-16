88d7108d4818599891815af29968828226eea272
Gustavo Larsen <41484330+gusgamedev@users.noreply.github.com>
2019-04-16 17:11:55 -0300

    Primeiro commit
    Criada a tela e iniciado a declaracao de variaveis na classe

--- app/src/main/java/games/gusdev/KAppTemperatureConverter/MainActivity.kt
+++ app/src/main/java/games/gusdev/KAppTemperatureConverter/MainActivity.kt
@@ -2,11 +2,22 @@ package games.gusdev.KAppTemperatureConverter
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.RadioButton
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var tempValue: EditText
+    lateinit var radioC: RadioButton
+    lateinit var radioF: RadioButton
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        tempValue = findViewById(R.id.valorTemp) as EditText
+        radioC = findViewById(R.id.radioCelcius) as RadioButton
+        radioF = findViewById(R.id.radioFahrenheit) as RadioButton
+
     }
 }
