75e5754c6a8f78e45f2cfef6bd42952a3007ff18
Gustavo Larsen <41484330+gusgamedev@users.noreply.github.com>
2019-04-17 10:03:23 -0300

    Funcao de conversão
    Adicionado metodo de conversao do valor da temperatura

--- app/src/main/java/games/gusdev/KAppTemperatureConverter/MainActivity.kt
+++ app/src/main/java/games/gusdev/KAppTemperatureConverter/MainActivity.kt
@@ -2,14 +2,17 @@ package games.gusdev.KAppTemperatureConverter
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.EditText
 import android.widget.RadioButton
+import kotlinx.android.synthetic.*
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var tempValue: EditText
     lateinit var radioC: RadioButton
     lateinit var radioF: RadioButton
+    lateinit var convButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,5 +22,21 @@ class MainActivity : AppCompatActivity() {
         radioC = findViewById(R.id.radioCelcius) as RadioButton
         radioF = findViewById(R.id.radioFahrenheit) as RadioButton
 
+        convButton = findViewById(R.id.button) as Button
+        convButton.setOnClickListener{ converter(it)}
+
+    }
+
+    fun converter(view: View) {
+
+        var temp : Double =  tempValue.text.toString().toDouble()
+
+        if (radioC.isChecked) {
+            temp = (temp - 32) * 5/9
+        } else if (radioF.isChecked) {
+            temp = (temp * 5/9 + 32)
+        }
+
+        tempValue.setText(temp.toString())
     }
 }
